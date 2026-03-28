d10fa422d94aa96d92f5a32820999ef86020351f
bilzeng <bilzeng@qq.com>
2026-03-28 09:10:56 +0800

    chore: remove unused src directory containing old implementation

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-# Neo4j Memory Module
-from .neo4j_memory import Neo4jMemory, get_memory, record_message, retrieve_context
-
-__all__ = ['Neo4jMemory', 'get_memory', 'record_message', 'retrieve_context']
